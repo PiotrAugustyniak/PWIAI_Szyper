32f618c9f1ee27c49925b141500bcda50ecf28d8
PiotrAugustyniak <piotr.augustyniak2000@gmail.com>
2019-09-26 20:24:23 +0200

    aktualizacja bledu 4_string.php

--- 4_string.php
+++ 4_string.php
@@ -1,9 +1,9 @@
 <?php
-$text = <<< TEKST;
+$text = <<< TEKST
   zsk - Zespół
   szkół
   komunikacji
-TEKST;
+  TEKST;
 echo "Przed uzyciem funkcji nl2br:<br>$text<br>"; //newlinetobreak
 echo "<br>Po uzyciu funkcji nl2br:<br> ";
 echo nl2br($text);
